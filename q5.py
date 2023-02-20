5ecaffb98e8d46d339115e43a6d22ab54dbb9aa3
JBourton <joshua.bourton16@gmail.com>
2023-02-20 15:07:36 +0000

    Function to determine num of unique elements

--- q5.py
+++ q5.py
@@ -1,4 +1,5 @@
 import itertools
+from collections import Counter
 
 # Below represents (v1 ∨ ¬v2) ∧ (¬v1 ∨ v3)
 clauses = [[1, -2], [-1, 3]]
@@ -6,12 +7,32 @@ clauses = [[1, -2], [-1, 3]]
 
 def simple_sat_solve(clause_set):
     pass
-    # 'split and simplify' method - try each variable in order
 
-    # Iterate through each clause in the clause set
-    i = 0
-    while i < len(clause_set):
-        pass
+    # Determine number of variables
+    print(unique_values(unique_values(clause_set)))
+
+    n = len(unique_values())
+
+    allorderings = list(itertools.product([False, True], repeat=n))
+    return allorderings
+
+
+# Determine number of unique values in the clause set
+def unique_values(clause_set):
+    # Generate list of clauses without negations
+    less_negations = clause_set
+    value = 0
+    while value < len(less_negations):
+        subvalue = 0
+        while subvalue < len(less_negations[value]):
+            if less_negations[value][subvalue] < 0:
+                less_negations[value][subvalue] = abs(less_negations[value][subvalue])
+            subvalue += 1
+        value += 1
+
+    # Generate list of unique values
+    uniqueVals = list(set(itertools.chain(*less_negations)))
+    return uniqueVals
 
 
 result = simple_sat_solve(clauses)
