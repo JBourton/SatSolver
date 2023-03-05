d0233f60df976e1ab2d089a7ca4e5cb519cefc10
JBourton <joshua.bourton16@gmail.com>
2023-03-05 16:07:26 +0000

    Add pseudocode for DP algorithm

--- q6.py
+++ q6.py
@@ -1,2 +1,5 @@
 def branching_sat_solve(clause_set, partial_assignment):
-    pass
+    # If clause_set contains no clauses it is SAT; if it contains empty clauses, UNSAT
+    if clause_set == []:
+        pass
+
