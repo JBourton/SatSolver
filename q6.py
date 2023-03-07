84a20233b044c7bf4ef1e75fcceebabaa69cf419
JBourton <joshua.bourton16@gmail.com>
2023-03-07 10:06:25 +0000

    Start recursion implementation

--- q6.py
+++ q6.py
@@ -1,28 +1,27 @@
-import copy
 def branching_sat_solve(partial_assignment, clause_set):
     # If clause set contains empty clauses, UNSAT
     if [] in clause_set:
         return False
     # If clause_set contains no clauses, SAT;
-    if not clause_set:
+    elif not clause_set:
         return partial_assignment
+    # Search for a valid interpretation via backtracking
+    else:
+        # Select a variable to eliminate
+        chosen_literal = abs(clause_set[0][0])
+        partial_assignment.append(chosen_literal)
 
-    # Select a variable to eliminate
-    chosen_literal = abs(clause_set[0][0])
-    partial_assignment.append(chosen_literal)
+        for clause in clause_set[:]:
+            # Remove clauses containing True instance of this variable
+            if chosen_literal in clause:
+                clause_set.remove(clause)
+            # Remove negation of this variable from all disjunctions
+            elif -chosen_literal in clause:
+                clause.remove(-chosen_literal)
 
-    # Create copy to allow deletion via iteration
-    for clause in clause_set[:]:
-        # Remove clauses containing True instance of this variable
-        if chosen_literal in clause:
-            clause_set.remove(clause)
-        # Remove negation of this variable from all disjunctions
-        elif -chosen_literal in clause:
-            clause.remove(-chosen_literal)
-            # Remove clause from original instead of copy
-    print(clause_set)
+        print(clause_set)
+        return branching_sat_solve(partial_assignment, clause_set)
 
-    # TODO: call function twice with chosen variable, and it's negation
     return False
 
 
