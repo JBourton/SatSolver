b30a70c41efa9a94607c27c55d120a17bca4a809
JBourton <joshua.bourton16@gmail.com>
2023-03-12 22:54:34 +0000

    Progress with second implementation of Q6

---
+++ q6 alt style.py
@@ -0,0 +1,65 @@
+# Given a clause set in CNF, return a satisfying partial assignment, or False is none exists
+def branching_sat_solve(partial_assignment, clause_set):
+    # Apply propagation to clause set
+    def reduce(original_clause_set, chosen_literal):
+        print("chosen literal: " + str(chosen_literal))
+        copy_set = original_clause_set[:]
+        for clause_line in copy_set[:]:
+            # Remove clauses containing True instance of this variable
+            if chosen_literal in clause_line:
+                copy_set.remove(clause_line)
+            # Remove negation of this variable from all disjunctions
+            elif -chosen_literal in clause_line:
+                clause_line.remove(-chosen_literal)
+        print("new_clause_set: " + str(copy_set))
+        return copy_set
+
+    def backtrack(updated_partial_assignment, original_clause_set):
+        print()
+        print("Current partial assignment: " + str(updated_partial_assignment))
+        # If clause_set contains no clauses, SAT;
+        if not original_clause_set:
+            print("clause set empty")
+            return updated_partial_assignment
+        # If clause set contains empty clauses, UNSAT
+        elif [] in original_clause_set:
+            print("[] in clause set")
+            return False
+
+        # Generate list of remaining unique literals
+        unique_set = set()
+        for clause in original_clause_set:
+            for literal in clause:
+                unique_set.add(abs(literal))
+        unique_literals = list(unique_set)
+        print("Unique literals: " + str(unique_literals))
+
+        for literal in unique_literals:
+            if literal not in updated_partial_assignment and -literal not in updated_partial_assignment:
+                # Branch on the 2 truth assignments for selected variable
+                for selected_literal in [literal, -literal]:
+                    if [] in original_clause_set:
+                        print("[] MOST DEFINITELY in clause set")
+                        break
+                    print("next")
+                    new_partial_assignment = updated_partial_assignment + [selected_literal]
+                    print("new_partial_assignment: " + str(new_partial_assignment))
+                    # Simplify expression with selected literal
+                    new_clause_set = reduce(original_clause_set, selected_literal)
+                    result = backtrack(new_partial_assignment, new_clause_set)
+                    if result is not False:
+                        print("up condition")
+                        return result
+
+    return backtrack(partial_assignment, clause_set)
+
+
+clauses = [[2, 3], [-1, -3], [-1, -2, 3], [4, 1, -3], [-4, 1, 3]]
+
+print("Original clause set: " + str(clauses))
+partial_assign = []
+satisfiability = branching_sat_solve(partial_assign, clauses)
+if not satisfiability:
+    print("UNSAT")
+else:
+    print("Satisfiable by: " + str(satisfiability))
--- q6.py
+++ q6.py
@@ -3,6 +3,7 @@ def branching_sat_solve(partial_assignment, clause_set):
     # Reduce clause set for selected variable
     if partial_assignment:
         chosen_literal = partial_assignment[-1]
+        print("Selected literal: " + str(chosen_literal))
         for clause in clause_set[:]:
             # Remove clauses containing True instance of this variable
             if chosen_literal in clause:
@@ -17,13 +18,14 @@ def branching_sat_solve(partial_assignment, clause_set):
         return True
     # If clause set contains empty clauses, UNSAT
     if [] in clause_set:
+        print("Going up recursion level with: " + str(partial_assignment) + " because of: " + str(clause_set))
         return False
 
     # Select new variable to eliminate
     chosen_literal = abs(clause_set[0][0])
     partial_assignment.append(chosen_literal)
-    print("Partial Assignment: " + str(partial_assignment))
     print("Clause Set: " + str(clause_set))
+    print("Partial Assignment: " + str(partial_assignment))
 
     # Branch on the 2 truth assignments for selected variable
     partial_assignment.append(chosen_literal)
@@ -42,8 +44,13 @@ def branching_sat_solve(partial_assignment, clause_set):
 # clauses = [[], [4, 5]]
 # clauses = [[1, -2], [1, 2], [-1, -2], [-1, 2]]
 # clauses = [[1, -2], [1, 2], [-1, -2], [-1, 2], [1]]
-clauses = [[-1, 2, 3], [1, 3, 4], [1, 3, -4], [1, -3, 4], [1, -3, -4], [-2, -3, 4], [-1, 2, -3], [-1, -2, 3]]
+# clauses = [[-1, 2, 3], [1, 3, 4], [1, 3, -4], [1, -3, 4], [1, -3, -4], [-2, -3, 4], [-1, 2, -3], [-1, -2, 3]]
 # clauses = [[1, 2, 3], [-1, 2, 3], [1, -2, 3], [1, 2, -3]]
+clauses = [[2, 3], [-1, -3], [-1, -2, 3], [4, 1, -3], [-4, 1, 3]]
 
 partial_assign = []
-print("Satisfiable by: " + str(branching_sat_solve(partial_assign, clauses)))
+satisfiability = branching_sat_solve(partial_assign, clauses)
+if not satisfiability:
+    print("UNSAT")
+else:
+    print("Satisfiable by: " + str(satisfiability))
