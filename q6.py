07cc8452c22fce0a5364ff801cdb4ac07b79e996
JBourton <joshua.bourton16@gmail.com>
2023-03-20 15:12:51 +0000

    Update q6 to reflect modifications

--- q6.py
+++ q6.py
@@ -18,28 +18,32 @@ def branching_sat_solve(partial_assignment, clause_set):
         print("new_clause_set: " + str(copy_set))
         return copy_set
 
-    # Search for a satisfying partial assignment
     def backtrack(updated_partial_assignment, original_clause_set):
         print()
-        # If clause_set contains no clauses, SAT;
+        # If clause set contains no clauses, SAT;
         if not original_clause_set:
-            print("Clause set empty, expression is SAT")
+            print("clause set empty, expression is SAT")
             return updated_partial_assignment
 
-        # Generate list of remaining unique literals
-        unique_set = set()
-        for clause in original_clause_set:
-            for literal in clause:
-                unique_set.add(abs(literal))
-        unique_literals = list(unique_set)
+        # Find most common element to branch on
+        # Credit: GeeksForGeeks for basic implementation of selecting the literal with the highest frequency
+        # appearance ~ https://www.geeksforgeeks.org/python-find-most-common-element-in-a-2d-list/
+        flattened_set = [lit for clause in original_clause_set for lit in clause]
+        available_literals = [lit for lit in flattened_set if -lit not in updated_partial_assignment and
+                              lit not in updated_partial_assignment]
+        if available_literals:
+            literal = max(available_literals, key=flattened_set.count)
+        else:
+            # Satisfying assignment found
+            return updated_partial_assignment
 
         # Try each literal and its negation at different levels
-        for literal in unique_literals:
-            both_assignments = [literal, -literal]
-            if both_assignments[0] not in updated_partial_assignment and both_assignments[
-                1] not in updated_partial_assignment:
-                # Branch on the 2 truth assignments for selected variable
-                for selected_literal in both_assignments:
+        both_assignments = [literal, -literal]
+        if both_assignments[0] not in updated_partial_assignment and \
+                both_assignments[1] not in updated_partial_assignment:
+            # Try each literal and its negation at different levels
+            for selected_literal in both_assignments:
+                if -literal not in updated_partial_assignment:
                     new_partial_assignment = updated_partial_assignment + [selected_literal]
                     print("new_partial_assignment: " + str(new_partial_assignment))
 
@@ -66,8 +70,10 @@ def branching_sat_solve(partial_assignment, clause_set):
 # clauses = [[-1, 2, 3], [1, 3, 4], [1, 3, -4], [1, -3, 4], [1, -3, -4], [-2, -3, 4], [-1, 2, -3], [-1, -2, 3]]
 # clauses = [[1, 2, 3], [-1, 2, 3], [1, -2, 3], [1, 2, -3]]
 clauses = [[2, 3], [-1, -3], [-1, -2, 3], [4, 1, -3], [-4, 1, 3]]
-# clauses = [[1], [1, 4, 5], [-1, -2], [-1, 3], [-3, 2, 6], [6, 2, -7, -4, 5], [-6, 2, -7, -8, 9], [-1, -2, 4, -5], [1, 2, 4, -8]]
-four_queens = [[-1, -5], [-2, -6], [-3, -7], [-4, -8], [-1, -9], [-2, -10], [-3, -11], [-4, -12], [-1, -13], [-2, -14],
+# clauses = [[1], [1, 4, 5], [-1, -2], [-1, 3], [-3, 2, 6], [6, 2, -7, -4, 5], [-6, 2, -7, -8, 9], [-1, -2, 4, -5],
+# [1, 2, 4, -8]]
+four_queens = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16], [17, 18, 19, 20], [-1, -5], [-2, -6],
+               [-3, -7], [-4, -8], [-1, -9], [-2, -10], [-3, -11], [-4, -12], [-1, -13], [-2, -14],
                [-3, -15], [-4, -16], [-1, -17], [-2, -18], [-3, -19], [-4, -20], [-5, -1], [-6, -2], [-7, -3], [-8, -4],
                [-5, -9], [-6, -10], [-7, -11], [-8, -12], [-5, -13], [-6, -14], [-7, -15], [-8, -16], [-5, -17],
                [-6, -18], [-7, -19], [-8, -20], [-9, -1], [-10, -2], [-11, -3], [-12, -4], [-9, -5], [-10, -6],
